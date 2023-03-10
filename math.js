fb4dafebdd250c7f6dce52e02fa8fdb6230fd56c
Augusto Monteiro <augustoengenharia@outlook.com>
2023-03-10 15:02:18 -0300

    Create math.js

---
+++ math.js
@@ -0,0 +1,16 @@
+//Utilizando alguns comandos Math
+
+//Math.random()
+console.log(Math.random())
+
+//Math.max(2,5,3,9,13,56)
+console.log(Math.max(2,5,3,9,13,56))
+
+//Math.min
+console.log(Math.min(2,5,3,9,13,56))
+
+//Math.E constante de Euler
+console.log(Math.E)
+
+//Math.PI
+console.log(Math.PI)
