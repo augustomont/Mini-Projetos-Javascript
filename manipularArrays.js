7c1eb38e7e17d4acbb7d75558745e57d64620cec
Augusto Monteiro <augustoengenharia@outlook.com>
2023-03-13 16:05:49 -0300

    Update manipularArrays.js

--- manipularArrays.js
+++ manipularArrays.js
@@ -1,7 +1,10 @@
 //Manipulando Arrays
 
+
+//Elementos primitivos
 const numeros = [1,2,3,4,5]
 console.log(numeros)
+
 
 //Adcionando elementos
 
@@ -16,3 +19,41 @@ console.log(numeros)
 //No final
 numeros.push('c')
 console.log(numeros)
+
+
+
+//Consultando elementos usando numero de referencia
+
+//saber em qual espaço esta localizado tal elemento
+console.log(numeros.indexOf('b'))
+
+//saber a ultima posiçaõ que apareceu um objeto que se repete dentro da array
+//Vou colocar mais um 'a'
+numeros.push('a')
+console.log(numeros.lastIndexOf('a'))
+
+//Saber se um elemento esta dentro da Array
+console.log(numeros.includes('a'))
+console.log(numeros.includes('f'))
+
+
+
+//Elementos de referencia
+
+const marcas = [
+    {id:1, nome:'a'},
+    {id:2, nome:'b'}
+]
+
+
+//Achar  um elemento dentro de uma arrays de objetos
+
+//usando o find(). criei a const só pra poder imprimir no console
+const achar = marcas.find(function(achar){
+    return achar.nome == 'b'
+})
+console.log(achar)
+
+//usando Arrow para achar
+const achar2 = marcas.find((achar2) => achar2.nome == 'a')
+console.log(achar2)
