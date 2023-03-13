89ee726fc08ad94b37d5e632229ae95ae72db2c2
Augusto Monteiro <augustoengenharia@outlook.com>
2023-03-13 14:03:34 -0300

    Create criadorPostagens.js

---
+++ criadorPostagens.js
@@ -0,0 +1,16 @@
+//Criando uma variavel pra postar uma mensagem automatica
+
+
+let postagem = {
+    titulo: 'a',
+    mensagem: 'b',
+    autor: 'c',
+    visualizacoes: 10,
+    comentarios: [
+        {autor: 'a',mensagem: 'b'},
+        {autor: 'a',mensagem: 'b'}
+    ],
+    estaAoVivo: true
+}
+
+console.log(postagem)
